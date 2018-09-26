11651b89dbb79c586130a9737507f950a535ed43
irintag <irinatagaevarene@mail.ru>
2018-09-26 18:29:23 +0600

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var str = require('./constant');
+var sec = require('../js/second');
+
+var App = function() {
+    document.write(str);
+    document.write('<br/>');
+    document.write(sec);
+};
+var app = new App();
+app();
+
+
